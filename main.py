51cfbbbed109b7d32f77b237cff4f5e3e6633220
priyamehta <mehta.priyar@northeastern.edu>
2025-11-13 19:46:47 -0500

    cors permisson update

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ app = FastAPI(title="Sentiment Aura API", version="1.0.0")
 # CORS middleware to allow frontend connections
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080", "http://localhost:5173", "http://127.0.0.1:8080"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
